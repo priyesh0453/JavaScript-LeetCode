3d96cb3c9534a1e339adf2046c09569374603793
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-05-29 20:06:57 +0530

    Create LeetCode 2629. Function Composition

---
+++ Easy/2629. Function Composition.js
@@ -0,0 +1,27 @@
+/*  2629. FUNCTION COMPOSITION: 
+    Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+    The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+    The function composition of an empty list of functions is the identity function f(x) = x.
+    You may assume each function in the array accepts one integer as input and returns one integer as output.
+*/
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+ 
+var compose = function(functions) 
+{
+	return functions.reduceRight((prevFn, currentFn) => x => currentFn(prevFn(x)), x => x);        
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+ 
+/*  EXTRAS:
+
+1) The "reduceRight" method is used to iterate over the array of functions in reverse order.
+
+*/
